95de7fb6fee7e594473601f0f9accff5cb6fec92
Nbazylevich <nbazylevich@gmail.com>
2017-09-24 00:09:25 +0300

    процесс работы

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,6 +1,15 @@
 angular.module('myApp', [])
 .controller('mainCtrl', function ($scope, $log) {
-
+    $scope.itemsWork = [
+        {indx: "01", date: "06 Сентября, 2017",
+            text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+        {indx: "02", date: "06 Сентября, 2017",
+            text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+        {indx: "03", date: "06 Сентября, 2017",
+            text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+        {indx: "04", date: "06 Сентября, 2017",
+            text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"}
+    ];
 });
 
 
