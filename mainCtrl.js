171c0cf039bc2f34a6d0bd3058b2d60839a32454
Nbazylevich <nbazylevich@gmail.com>
2017-09-26 23:19:42 +0300

    Процесс работы: добавила стрелочки

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,16 +1,56 @@
 angular.module('myApp', [])
-.controller('mainCtrl', function ($scope, $log) {
-    $scope.itemsWork = [
-        {indx: "01", date: "06 Сентября, 2017",
-            text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
-        {indx: "02", date: "06 Сентября, 2017",
-            text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
-        {indx: "03", date: "06 Сентября, 2017",
-            text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
-        {indx: "04", date: "06 Сентября, 2017",
-            text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"}
-    ];
-});
+    .controller('mainCtrl', function ($scope, $log) {
+        $scope.startIndx = 0;
+        $scope.endIndx = 3;
+        $scope.itemsWork = [
+            {indx: "01", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "02", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "03", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "04", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "05", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "06", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "07", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "08", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"}
+        ];
+        $scope.itemsWorkVisible = $scope.itemsWork.slice($scope.startIndx, 4);
+        $log.log("$scope.itemsWorkVisible = ", $scope.itemsWorkVisible)
+
+
+        $scope.clickRight = function () {
+            $log.log("clickRight $scope.startIndx = ", $scope.startIndx)
+//            var indx = 0;
+//            for(var i = $scope.endIndx; i < ($scope.endIndx + 4); i++){
+//                if($scope.itemsWork[i]){
+//                    indx = i + 1;
+//                }
+//                $log.log("indx = ", indx)
+//            }
+//            $scope.startIndx += indx;
+//            $scope.endIndx += indx;
+//            $log.log("clickRight $scope.startIndx = ", $scope.startIndx)
+//            $scope.itemsWorkVisible = $scope.itemsWork.slice($scope.startIndx, $scope.startIndx + 4);
+        };
+        $scope.clickLeft = function () {
+            $log.log("clickLeft $scope.startIndx = ", $scope.startIndx)
+//            var indx = 0;
+//            for(var i = ($scope.startIndx - 4); i < $scope.startIndx; i++){
+//                if($scope.itemsWork[i]){
+//                    indx = i - 1;
+//                }
+//                $log.log("indx = ", indx)
+//            }
+//            $scope.startIndx -= indx;
+//            $scope.itemsWorkVisible = $scope.itemsWork.slice($scope.startIndx, $scope.startIndx - 4);
+        };
+    });
 
 
 
