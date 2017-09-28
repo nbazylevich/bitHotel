050933bf3dbf35c163fbfe5dc8cbe6de15fa8837
Nbazylevich <nbazylevich@gmail.com>
2017-09-28 21:08:48 +0300

    процесс работы - обработка стрелок для листания

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,7 +1,7 @@
 angular.module('myApp', [])
     .controller('mainCtrl', function ($scope, $log) {
         $scope.startIndx = 0;
-        $scope.endIndx = 3;
+        $scope.endIndx = 4;
         $scope.itemsWork = [
             {indx: "01", date: "06 Сентября, 2017",
                 text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
@@ -18,37 +18,27 @@ angular.module('myApp', [])
             {indx: "07", date: "06 Сентября, 2017",
                 text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
             {indx: "08", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "09", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "10", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "11", date: "06 Сентября, 2017",
+                text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"},
+            {indx: "12", date: "06 Сентября, 2017",
                 text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising"}
         ];
         $scope.itemsWorkVisible = $scope.itemsWork.slice($scope.startIndx, 4);
-        $log.log("$scope.itemsWorkVisible = ", $scope.itemsWorkVisible)
-
 
         $scope.clickRight = function () {
-            $log.log("clickRight $scope.startIndx = ", $scope.startIndx)
-//            var indx = 0;
-//            for(var i = $scope.endIndx; i < ($scope.endIndx + 4); i++){
-//                if($scope.itemsWork[i]){
-//                    indx = i + 1;
-//                }
-//                $log.log("indx = ", indx)
-//            }
-//            $scope.startIndx += indx;
-//            $scope.endIndx += indx;
-//            $log.log("clickRight $scope.startIndx = ", $scope.startIndx)
-//            $scope.itemsWorkVisible = $scope.itemsWork.slice($scope.startIndx, $scope.startIndx + 4);
+            $scope.startIndx += 4;
+            $scope.endIndx += 4;
+            $scope.itemsWorkVisible = $scope.itemsWork.slice($scope.startIndx, $scope.endIndx);
         };
         $scope.clickLeft = function () {
-            $log.log("clickLeft $scope.startIndx = ", $scope.startIndx)
-//            var indx = 0;
-//            for(var i = ($scope.startIndx - 4); i < $scope.startIndx; i++){
-//                if($scope.itemsWork[i]){
-//                    indx = i - 1;
-//                }
-//                $log.log("indx = ", indx)
-//            }
-//            $scope.startIndx -= indx;
-//            $scope.itemsWorkVisible = $scope.itemsWork.slice($scope.startIndx, $scope.startIndx - 4);
+            $scope.startIndx -= 4;
+            $scope.endIndx -= 4;
+            $scope.itemsWorkVisible = $scope.itemsWork.slice($scope.startIndx, $scope.endIndx);
         };
     });
 
